11b912caae38833e95021ec4cfad149d850b2426
mukulbhambri1 <63150030+mukulbhambri1@users.noreply.github.com>
2021-04-11 11:09:09 +0530

    Create iot.php

---
+++ iot.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+
+<?php echo $_POST["name"]; 
+if($_POST["name"]=="μTMapS")
+{
+	print('<h1>μTMaps and μSTMaps are robust high temperature sensors which are capable of multi-point temperature measurement using a single waveguide sensor.</h1>');
+}
+
+if($_POST["name"]=="PoRTS")
+{
+	print('<h1> PoRTS is a our multi-parameter sensor which measures the Viscosity, Temperature and Level of a fluid using a single ultrasonic waveguide with higher reliability and precision.</h1>');
+}
+
+if($_POST["name"]=="Ztar")
+{
+	print('<h1>Ztar is a waveguide based ultrasonic level sensor which provides accurate measurement even at higher temperatures. It has both contact and non-contact variants </h1>');
+}
+
+if($_POST["name"]=="I-PAMS")
+{
+	print('<h1>Industrial-IoT based asset monitoring system providing real time data visualization and detailed analysis. </h1>');
+}
+
+?>
+
+
+</body>
+</html>
